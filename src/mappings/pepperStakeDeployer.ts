928a5c90b0343db54b3881d3d30bef516494ca52
Jonathan Miller <jon@jmill.dev>
2022-11-21 09:03:13 -0500

    New subgraph baybeeeee

--- src/mappings/pepperStakeDeployer.ts
+++ src/mappings/pepperStakeDeployer.ts
@@ -1,4 +1,4 @@
-import { Bytes } from "@graphprotocol/graph-ts";
+import { BigDecimal, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { DeployPepperStake } from "../../generated/PepperStakeDeployer/PepperStakeDeployer";
 import {
   DeployPepperStakeEvent,
@@ -6,12 +6,18 @@ import {
 } from "../../generated/schema";
 import { PepperStake } from "../../generated/templates";
 
+const MAX_FEE = BigInt.fromI32(1_000_000_000);
+
+function getFeePercentage(fee: BigInt): BigDecimal {
+  return fee.div(MAX_FEE).toBigDecimal();
+}
+
 export function handleDeployPepperStake(event: DeployPepperStake): void {
   let deployPepperStakeEvent = new DeployPepperStakeEvent(
-    event.transaction.hash.toHexString().toLowerCase()
+    event.transaction.hash
   );
   if (deployPepperStakeEvent) {
-    deployPepperStakeEvent.timestamp = event.block.timestamp.toI32();
+    deployPepperStakeEvent.timestamp = event.block.timestamp;
     deployPepperStakeEvent.txHash = event.transaction.hash;
 
     PepperStake.create(event.params.pepperStake);
@@ -19,25 +25,56 @@ export function handleDeployPepperStake(event: DeployPepperStake): void {
   let pepperStakeContract = new PepperStakeContract(
     event.params.pepperStake.toHexString()
   );
+
   if (pepperStakeContract) {
-    pepperStakeContract.supervisors = changetype<Bytes[]>(
-      event.params._supervisors
-    );
-    pepperStakeContract.unreturnedStakeBeneficiaries = changetype<Bytes[]>(
-      event.params._unreturnedStakeBeneficiaries
-    );
     pepperStakeContract.address = event.params.pepperStake;
     pepperStakeContract.creator = event.transaction.from;
-    pepperStakeContract.stakeAmount = event.params._stakeAmount;
-    pepperStakeContract.returnWindowDays = event.params._returnWindowDays;
-    pepperStakeContract.maxParticipants = event.params._maxParticipants;
+    pepperStakeContract.projectId = event.params.projectId.toI32();
+
+    pepperStakeContract.supervisors = changetype<Bytes[]>(
+      event.params.launchData.supervisors
+    );
+    pepperStakeContract.participantAllowList = changetype<Bytes[]>(
+      event.params.launchData.participantAllowList
+    );
+    pepperStakeContract.unreturnedStakeBeneficiaries = changetype<Bytes[]>(
+      event.params.launchData.unreturnedStakeBeneficiaries
+    );
+    pepperStakeContract.oracleDelegateAddresses = changetype<Bytes[]>(
+      event.params.launchData.oracleDelegateAddresses
+    );
+    pepperStakeContract.stakingTiers = event.params.launchData.stakingTiers;
+
+    pepperStakeContract.completionWindowSeconds =
+      event.params.launchData.completionWindowSeconds;
+    pepperStakeContract.maxParticipants =
+      event.params.launchData.maxParticipants;
+    pepperStakeContract.shouldUseParticipantAllowList =
+      event.params.launchData.shouldUseParticipantAllowList;
     pepperStakeContract.shouldParticipantsShareUnreturnedStake =
-      event.params._shouldParticipantsShareUnreturnedStake;
+      event.params.launchData.shouldParticipantsShareUnreturnedStake;
     pepperStakeContract.shouldUseSupervisorInactionGuard =
-      event.params._shouldUseSupervisorInactionGuard;
-    pepperStakeContract.metadataURI = event.params._metadataURI;
+      event.params.launchData.shouldUseSupervisorInactionGuard;
+
+    pepperStakeContract.creatorFeePercentage = getFeePercentage(
+      event.params.launchData.creatorFee
+    );
+    pepperStakeContract.creatorFeeBeneficiary =
+      event.params.launchData.creatorFeeBeneficiary;
+    pepperStakeContract.supervisorTipPercentage = getFeePercentage(
+      event.params.launchData.supervisorTip
+    );
+    pepperStakeContract.protocolFeePercentage = getFeePercentage(
+      event.params.protocolFee
+    );
+    pepperStakeContract.protocolFeeBeneficiary =
+      event.params.protocolFeeBeneficiary;
+
+    pepperStakeContract.metadataURI = event.params.launchData.metadataURI;
     pepperStakeContract.save();
   }
+
+  // Finally, save the event
   deployPepperStakeEvent.pepperStakeContract = pepperStakeContract.id;
   deployPepperStakeEvent.save();
 }
